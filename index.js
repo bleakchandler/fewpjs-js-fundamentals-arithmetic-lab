ae165576ba770916b75480c50b316b268ee6db44
Bleak Chandler <bleakchandler@gmail.com>
2021-03-15 23:59:40 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
-// Write your code here
+// Write your code herevar num1 = 31
+
+var num1 = 31
+var num2 = 2
+var num3 = 104
+var num4 = 25
+
+var multiply = num1 * num2
+
+var random = Math.floor(Math.random() * 10) + 1;
+
+var mod = num3 % num4
+
+var max = Math.max((1, 2, 5, 10, 20))
+
